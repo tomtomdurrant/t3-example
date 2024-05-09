7fd106898515bf54c015e4ad4f103ee4b35f0427
Tom Durrant <tomchdurrant@gmail.com>
2024-05-09 17:58:29 +0100

    Use github

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,9 +48,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
